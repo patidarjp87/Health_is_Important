2d94c3a13dee2e97f76f0a72c46e599b3f8a105c
jaydeep <jaydeeppatidar87@gmail.com>
2020-08-06 12:35:17 +0530

    updated a file delete some files

--- health.py
+++ health.py
@@ -1,39 +1,53 @@
 from pygame import mixer
 from datetime import datetime
 from time import time
-def musiconloop(file,stopper):
+
+def musiconloop(file, stopper):
     mixer.init()
     mixer.music.load(file)
     mixer.music.play()
     while True:
-        a=input()
-        if a==stopper:
+        input_of_user = input()
+        if input_of_user == stopper:
             mixer.music.stop()
             break
+
 def log_now(msg):
-    with open('mylogs.txt','a') as f:
+    with open("mylogs.txt", "a") as f:
         f.write(f"{msg} {datetime.now()}\n")
-if __name__=='__main__':
-    init_water=time()
-    init_eyes=time()
-    init_physical=time()
-    watersecs=2400
-    eyessecs=1800
-    exercisesecs=3000
+
+if __name__ == '__main__':
+    # musiconloop("water.mp3", "stop")
+    init_water = time()
+    init_eyes = time()
+    init_exercise = time()
+    watersecs = 60
+    exsecs = 120
+    eyessecs = 45
+
     while True:
-        if time()-init_water > watersecs:
-            print("water drinking time.Enter 'drank' to stop alarm")
-            musiconloop('water.mp3','drank')
-            init_water=time()
-            log_now('Drank water at')
-        if time()-init_eyes > eyessecs:
-            print("Eyes exercise time.Enter 'done' to stop alarm")
-            musiconloop('eyes.mp3','done')
-            init_eyes=time()
-            log_now('Done eyes exercise at')
-        if time()-init_physical > exercisesecs:
-            print("normal exercise time.Enter 'done' to stop alarm")
-            musiconloop('physical.mp3','done')
-            init_physical=time()
-            log_now('Done normal exercise at')
-            
+        if time() - init_water > watersecs:
+            print("Water Drinking time. Enter 'drank' to stop the alarm.")
+            musiconloop('water.mp3', 'drank')
+            init_water = time()
+            log_now("Drank Water at")
+
+        if time() - init_eyes >eyessecs:
+            print("Eye exercise time. Enter 'doneeyes' to stop the alarm.")
+            musiconloop('eyes.mp3', 'doneeyes')
+            init_eyes = time()
+            log_now("Eyes Relaxed at")
+
+        if time() - init_exercise > exsecs:
+            print("Physical Activity Time. Enter 'donephy' to stop the alarm.")
+            musiconloop('physical.mp3', 'donephy')
+            init_exercise = time()
+            log_now("Physical Activity done at")
+
+
+#Healthy Programmer
+# Water - water.mp3 (3.5 litres) - Drank - log - Every 50 min
+# Eyes - eyes.mp3 - every 30 min - EyDone - log - Every 40 min
+# Physical activity - physical.mp3 every - 55 min - ExDone - log
+
+
